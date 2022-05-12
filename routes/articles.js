4c6ef9dfa5fffe33d7b54fe7f3d33a17c62fd47c
Sinem <coskunsiinem@gmail.com>
2022-05-12 22:55:58 +0300

    articles.js connect

---
+++ routes/articles.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send('articles burada')
+})
+
+
+
+
+module.exports = router // her yerde rahatlıkla kullanabilmemiz için module.export önemlidir.
